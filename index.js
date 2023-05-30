7b6cdf5f8b9744c093349a24e02c1a9c2a95685c
Isam Nagi <isam.nagi@gmail.com>
2023-05-30 12:36:23 -0400

    feat: Complete auth action to register user and saveto db and updated index Files changed  - index            -defined route for auth.js and import statement for register info from auth in controllers  - controllers/auth            -setup register user const which allowed for storing all info of user and hashing password of new user and saving user to db

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -34,6 +35,8 @@ app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
     });
     const upload = multer({ storage });
 
+    /* ROUTES WITH FILES */
+    app.post("/auth/register", upload.single("picture"), register);
 
     /*MONGOOSE SETUP*/
     const PORT = process.env.PORT || 6001;
