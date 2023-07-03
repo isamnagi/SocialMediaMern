7e8a5c3095d47e2c2b9e9e73827dbbfa2d976500
Isam Nagi <isam.nagi@gmail.com>
2023-07-03 17:07:06 -0400

    Add index for all scenes and begin app.js development with it's imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
-import './App.css';
+import { Router } from 'express';
+import { BrowserRouter, Navigate, routes, route } from 'react-router-dom';
+import HomePage from 'scenes/homePage';
+import LoginPage from 'scenes/loginPage';
+import ProfilePage from 'scenes/profilePage';
+
 
 function App() {
-    <div className="app"></div>
+    <div className="app">
+        <BrowserRouter>
+            <Router>
+                
+            </Router>
+        </BrowserRouter>
+    </div>
 }
 
 export default App;
