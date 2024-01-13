67664ca7b055f329f996641e1109263105ca8880
Isam Nagi <isam.nagi@gmail.com>
2024-01-13 08:39:39 -0500

    Complete client/src/state and work on  /client/src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,26 @@ import { BrowserRouter, Navigate, routes, route } from 'react-router-dom';
 import HomePage from 'scenes/homePage';
 import LoginPage from 'scenes/loginPage';
 import ProfilePage from 'scenes/profilePage';
-
+import { useMemo } from "react";
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from 'theme';
 
 function App() {
-    <div className="app">
-        <BrowserRouter>
-            <Router>
-                <Route path="/" element={<LoginPage />} />
-                <Route path="/home" element={<HomePage />} />
-                <Route path="/profile/:userId" element={<ProfilePage />} />
-            </Router>
-        </BrowserRouter>
-    </div>
+    const mode = useSelector((state) => state.mode);
+
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <Router>
+                    <Route path="/" element={<LoginPage />} />
+                    <Route path="/home" element={<HomePage />} />
+                    <Route path="/profile/:userId" element={<ProfilePage />} />
+                </Router>
+            </BrowserRouter>
+        </div>
+    )
 }
 
 export default App;
