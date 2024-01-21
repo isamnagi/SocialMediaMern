c9dcae0e5756b5356257a083f228b77c2e685d6c
Isam Nagi <isam.nagi@gmail.com>
2024-01-21 15:05:46 -0500

    complete register login page

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,4 +1,5 @@
 import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
 
 const LoginPage = () => {
     const theme = useTheme();
@@ -22,9 +23,14 @@ const LoginPage = () => {
         <Box
         width={isNonMobileScreens ? "50%" : "93%"}
         p="2rem"
-        m="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
         >
-
+            <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem"}}>
+            Welcome to Isam's Social Media webiste, a site for social on media 
+            </Typography>
+            <Form />
         </Box>
     </Box>
     return <div>loginpage</div>
