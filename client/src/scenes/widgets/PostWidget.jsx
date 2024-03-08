185f3f8da65069b660094647fd45bc548ae23fef
Isam Nagi <isam.nagi@gmail.com>
2024-03-08 02:54:12 -0500

    Complete Freind componenet, PostWidget

---
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,74 @@
+import { 
+    ChatBubbleOutlineOutlined, 
+    FavoriteBorderOutlines, 
+    FavoriteOutlined,
+    ShareOutlined 
+} from "@mui/icons-material";
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import Friend from "components/Friend";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost } from "state";
+
+const PostWidget = ({
+    postId,
+    postUserId,
+    name,
+    description,
+    location,
+    picturePath,
+    userPicturePath,
+    likes,
+    comments,
+}) => {
+    const [isComments, setIsComments] = useState(false);
+    const dispatch = useDispatch();                                          
+    const token = useSelector((state) => state.token);
+    const loggedInUserId = useSelector((state) => state.user._id);
+    const isLiked = Boolean(likes[loggedInUserId]);
+    const likeCount = Object.keys(likes).length
+
+    const { palatte } = useTheme();
+    const main = palatte.neutral.main;
+    const primary = palatte.primary.medium;
+
+    const patchLike = async () => {
+        const response = await fetch(`http://localhost:3001/posts/${postId}/like`,  {
+            method: "PATCH",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application.json",
+            },
+            body: JSON.stringify({ userId: loggedInUserId })
+        });
+        const updatedPost = response.json();
+        dispatch(setPost({ post: updatedPost }));
+    };
+}
+
+return (
+    <WidgetWrapper m="2rem 0">
+        <Friend
+            friendId={postUserId}
+            name={name}
+            subtitle={location}
+            userPicturePath={userPicturePath}
+        />
+        <Typography color={main} sx={{ mt: "1rem"}}>
+            {description}
+        </Typography>
+        {picturePath && (
+            <img 
+                width="100%"
+                height="auto"
+                alt="post"
+                style={{borderRadius:"0.75rem", marginTop:"0.75rem"}}
+                src={`http://localhost:3001/assests/${picturePath}`}
+            />
+        )}
+    </WidgetWrapper>
+)
+
+export default PostWidget;
