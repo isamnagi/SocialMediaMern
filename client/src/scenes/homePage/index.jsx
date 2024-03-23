f584a67e227e201672bd27f3f82beb10119dbe4f
Isam Nagi <isam.nagi@gmail.com>
2024-03-23 13:56:27 -0400

    Complete AdvertWidget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -5,6 +5,7 @@ import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostWidget from "scenes/widgets/PostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
+import AdvertWidget from "scenes/widgets/AdvertWidget";
 
 const HomePage = () => {
     const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -30,7 +31,11 @@ const HomePage = () => {
                     <MyPostWidget picturePath={picturePath} />
                     <PostsWidget userId={_id} />
                 </Box>
-                {isNonMobileScreens && <Box flexBasis="26%"></Box>}
+                {isNonMobileScreens && (
+                <Box flexBasis="26%">
+                    <AdvertWidget />
+                </Box>
+                )}
             </Box>
         </Box>
     );
